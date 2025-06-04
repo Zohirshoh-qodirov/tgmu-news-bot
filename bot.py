9932e4c2755c79373769705200909b1301a9112e
Zohirshoh-qodirov <zohirshohqodirov376@gmail.com>
2025-06-04 22:19:58 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,21 +2,16 @@ from aiogram import Bot, Dispatcher, executor, types
 from dotenv import load_dotenv
 import os
 
-# Загружаем переменные окружения из файла .env
-load_dotenv()
+load_dotenv()  # Загружаем .env
 
-# Получаем токен из переменной окружения
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Получаем токен из переменной окружения
 
-# Инициализируем бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-# Обработчик команды /start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("Привет! Я бот, который публикует новости!")
 
 if __name__ == '__main__':
-    # Запускаем бота
     executor.start_polling(dp, skip_updates=True)
